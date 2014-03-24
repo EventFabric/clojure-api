581a7dfd66cf89dd38887ca259373023024de9c4
Mariano Guerra <luismarianoguerra@gmail.com>
2014-03-24 12:39:48 +0100

    bump api version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-fabric/api-client "0.1.0"
+(defproject event-fabric/api-client "0.1.1"
   :description "Clojure library for Event Fabric's API"
   :url "http://github.com/EventFabric/clojure-client"
   :license {:name "MIT"
